cfb0c6679bfa9f49ab9eb061ddca1ac969fc0e84
Julio Castillo <juliocastillo@MacBook-Pro-de-Julio-2.local>
2023-11-10 15:58:54 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          🚧 <strong>juliocas.com</strong> is under construction now using React.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
